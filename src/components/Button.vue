c6cac5a9e840d01467d3482b10354143e3e3f33a
Banderas <djbanderas12russ@gmail.com>
2026-03-30 18:07:04 +0300

    2-button

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <button>Начать игру</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 150%;
+  text-align: center;
+  color: #fff;
+  width: 335px;
+  height: 68px;
+  background: #008bfe;
+  border-radius: 100px;
+  border: 0;
+  cursor: pointer;
+}
+</style>
